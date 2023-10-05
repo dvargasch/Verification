bb2452e9f45175cd9303fab11fb2862e862fd83d
reimorales1 <100234823+reimorales1@users.noreply.github.com>
2023-10-05 14:47:51 -0600

    Score con agente y monitor

--- Proyecto1/Scoreboard.sv
+++ Proyecto1/Scoreboard.sv
@@ -1,28 +1,74 @@
-class checker_scoreboard;
+class ag_chckr #(parameter ancho_pal = 16);
+  bit [ancho_pal-9:0] dato ;
+  bit [7:0] id ;
+  int tiempo;
+
+  
+  function new(bit [ancho_pal-1:0] , [7:0] direccion, tiempo, source);
+    this.dato = info;
+    this.id = direccion;
+    this.tiempo = tiempo;
+  endfunction
+  
+  function display();
+    $display("El dato: %b se envió, en el tiempo %g", this.dato, this.tiempo);
+  endfunction 
+  
+endclass
+
+class checker_sb #(parameter bits = 1, parameter terminales=5, parameter ancho_pal = 32);
   
   //Mailbox
   mntr_chckr_mbx #(.ancho_pal(ancho_pal)) mntr_chckr_mbx;
   
+  ag_chckr_mbx #(.ancho_pal(ancho_pal)) ag_chckr_mbx;
+  
+  
   //Transacción
-  mntr_chckr #(.ancho_pal(ancho_pal)) transaccion;
-  mntr_chckr instruccion [$];// esta es la estructura dinámica que maneja el scoreboard
+  mntr_chckr #(.ancho_pal(ancho_pal)) mntr_chckr_transaccion;
+  
+  ag_chckr	#(.ancho_pal(ancho_pal)) ag_chckr_transaccion;
+  
+ ///////////////////////  
+  
+  mntr_chckr #(.ancho_pal(ancho_pal)) recibida [$];
+ 
+  ag_chckr	#(.ancho_pal(ancho_pal)) entregada[$];
+  
+  
+  ///////////////////
+  // esta es la estructura dinámica que maneja el scoreboard
   
  
   
   function new();
-    this.instruccion= {};
+    this.recibida= {};
+    this.entregada= {};
+    this.mntr_chckr_mbx=new();
+    this.ag_chckr_mbx=new();
+    
   endfunction 
   
-  task run();
+  task run(); //Scoreboard_mon
     forever begin
       
-    this.mntr_chckr_mbx.get(this.transaccion);
-      this.instruccion.push_back(this.transaccion);
+    this.mntr_chckr_mbx.get(this.mntr_chckr_transaccion);
+      this.recibida.push_back(this.mntr_chckr_transaccion);
       
-      $display(" Se recibió el dato %b  con id %b con un tiempo de envío de [%g]",this.transaccion.dato,this.transaccion.id, this.transaccion.tiempo);
+      $display(" Se recibió el dato %b  con id %b con un tiempo de envío de 	[%g]",this.mntr_chckr_transaccion.dato,this.mntr_chckr_transaccion.id,this.mntr_chckr_transaccion.tiempo);
     end 
   endtask
   
+
+task run(); // Scoreboard agente
+    forever begin
+      
+    this.ag_chckr_mbx.get(this.ag_chckr_transaccion);
+      this.entregada.push_back(this.ag_chckr_transaccion);
+      
+      $display(" Se recibió el dato %b  con id %b con un tiempo de envío de [%g]",this.ag_chckr_transaccion.dato,this.ag_chckr_transaccion.id, this.ag_chckr_transaccion.tiempo);
+    end 
+  endtask
   
   
 endclass
