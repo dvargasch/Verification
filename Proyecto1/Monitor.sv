ffcb84afbedddeaf55b5477977ec3d70f56d4758
reimorales1 <100234823+reimorales1@users.noreply.github.com>
2023-10-06 12:33:52 -0600

    Monitor.sv listo y comentado

--- Proyecto1/Monitor.sv
+++ Proyecto1/Monitor.sv
@@ -1,3 +1,4 @@
+// Definición de la clase `fifo_monitor`
 class fifo_monitor #(parameter bits = 1, parameter terminales=5, parameter ancho_pal = 32);
   bit pop;
   bit push;
@@ -6,6 +7,8 @@ class fifo_monitor #(parameter bits = 1, parameter terminales=5, parameter ancho
   bit [ancho_pal-1:0] queue [$];
   int iterador;
   virtual interfaz #(.ancho_pal(ancho_pal), .terminales(terminales)) vif;
+  
+  // Constructor de la clase
   
   function new(int identify);
     this.pop = 0;
@@ -16,7 +19,9 @@ class fifo_monitor #(parameter bits = 1, parameter terminales=5, parameter ancho
     this.iterador = identify;
   endfunction 
   
-  task pending_update(); //Actualizacion del pending que sale de una FIFO hacia el Bus de datos
+   //Actualizacion del pending que sale de una FIFO hacia el Bus de datos
+  
+  task pending_update();
     forever begin
       @(negedge vif.clk);
       vif.pndng[0][iterador] = pndng; 
@@ -24,7 +29,9 @@ class fifo_monitor #(parameter bits = 1, parameter terminales=5, parameter ancho
     end
   endtask
   
-  task Dout_uptate(); // Visto desde la FIFO: actualiza el valor de salida de la fifo (o sea el valor de entrada del bus) y el valor de pending 
+  // Visto desde la FIFO: actualiza el valor de salida de la fifo (o sea el valor de entrada del bus) y el valor de pending
+  
+  task Dout_uptate();  
     forever begin
       @(posedge vif.clk);
       vif.D_pop[0][iterador] = queue[0]; // Indica que el dato de entrada al bus de datos va a estar almacenado en la posicion 
@@ -37,6 +44,7 @@ class fifo_monitor #(parameter bits = 1, parameter terminales=5, parameter ancho
     end
   endtask
   
+  // Método para actualizar el dato de entrada
   
   function void Din_update(bit [ancho_pal-1:0] dato); 
     queue.push_back(dato);    //Ingresa el dato en la fifo.
@@ -47,7 +55,10 @@ endclass
 
 ///////////////////////////////////////////
 
-class mntr_chckr_#(parameter ancho_pal=32);
+
+// Definición de la clase `mntr_chckr`
+
+class mntr_chckr #(parameter ancho_pal=32);
   reg [7:0] id;
   int dato;
   int tiempo;
@@ -56,6 +67,9 @@ class mntr_chckr_#(parameter ancho_pal=32);
   function new ();
   endfunction;
 endclass
+
+
+// Definición de la clase `monitor`
 
 class monitor #(parameter ancho_pal = 32,parameter bits=1, parameter terminales = 5);
   
@@ -70,47 +84,39 @@ class monitor #(parameter ancho_pal = 32,parameter bits=1, parameter terminales 
   
  int ident;    
   
+   // Constructor de la clase
+  
   function new(int id);
     this.ident = id;
     this.ff = new(id);
   endfunction
 
+   // Tarea principal del monitor
+  
   task run();
-    $display("[%g] Monitor # [%g] Inicia", $time, ident);
+    $display("[%g] Monitor # [%g] Inicia", $time, ident); // Imprime un mensaje indicando el inicio del monitor y la marca de tiempo actual
 
-    forever begin
-     //this.transaccion_in = new ();
-       this.transaccion = new ();
+    forever begin  // Bucle infinito
+      this.transaccion = new ();  // Crear una nueva instancia de la transacción
 
-      fork
+      fork // Fork para paralelizar dos tareas
         @(posedge ff.vif.clk) // Esperar al flanco de subida del reloj
-        ff.pushf();
-     	ff.update();
+        ff.pushf(); 
+        ff.update(); 
+      // Actualizar la FIFO (pop y actualizar Dout)
       join_none
  
       @(posedge ff.vif.clk);
-      if (ff.pndng == 1) begin
+      if (ff.pndng == 1) begin // Verificar si hay una transacción pendiente en la FIFO
+         // Establecer el tiempo y la identificación de la transacción
         this.transaccion.tiempo = $time;
         this.transaccion.id = ident;
         @(posedge ff.vif.clk);
-        this.transaccion.dato = ff.D_pop;
-        this.mntr_chckr_mbx.put(this.transaccion);
+        this.transaccion.dato = ff.D_pop;  // Establecer el tiempo y la identificación de la transacción
+        this.mntr_chckr_mbx.put(this.transaccion);  // Colocar la transacción en el mailbox
         transaccion_in.print("Monitor: Transaccion recibida");
       end
   
-      //@(posedge ff.vif.clk);// Monitorear datos que salen
-      //this.transaccion_out = new ();
-  
-      //@(posedge ff.vif.clk);
-      //if (ff.pndng == 1) begin
-        //ff.popf();
-        //this.transaccion_out.tiempo = $time;
-        //this.transaccion_out.id = ident;
-        //@(posedge fifo.vif.clk);
-        //this.transaccion_out.dato = ff.D_pop;
-        //this.mntr_chckr_mbx.put(this.transaccion_out);
-        //transaccion_out.print("Monitor: Transaccion enviada");
-      //end
       @(posedge ff.vif.clk);
     end
   endtask
