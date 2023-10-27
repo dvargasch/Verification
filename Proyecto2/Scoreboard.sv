b850cda361aa7e5172ade52468da6141a1d47bdf
reimorales1 <100234823+reimorales1@users.noreply.github.com>
2023-10-27 16:14:26 -0600

    Scoreboard.sv

--- Proyecto2/Scoreboard.sv
+++ Proyecto2/Scoreboard.sv
@@ -2,17 +2,17 @@
 
 class scoreboard #( parameter ROWS = 4, parameter COLUMS = 4, parameter pckg_sz = 40);
   
-  // Instancias de las mailboxes
+  ///////////////Mailboxes////////////////////
+  
   mntr_score_mbx mntr_score_mbx;
   
-  //drv_chckr_mbx drv_chckr_mbx;
-  
-  //ag_chckr_mbx #(.ancho_pal(ancho_pal)) ag_chckr_mbx;
+  drv_chckr_mbx  drv_chckr_mbx;
+ 
  
   // Instancias de las transacciones
   mntr_score  mntr_score_transaccion;
   
-  //drv_chckr	 drv_chckr_transaccion;
+  drv_chckr	 drv_chckr_transaccion_;
   
   
 // Arreglos dinámicos para transacciones recibidas y entregadas
@@ -21,24 +21,24 @@ class scoreboard #( parameter ROWS = 4, parameter COLUMS = 4, parameter pckg_sz 
   
   mntr_score #(.pckg_sz (pckg_sz ),.ROWS(ROWS),.COLUMS(COLUMS)) arr_rec [int]; 
  
-  //drv_chckr	#(.pckg_sz (pckg_sz ),.rows(rows),.columns(columns)) entregada[$];
- 
+  drv_chckr	#(.pckg_sz (pckg_sz ),.ROWS(ROWS),.COLUMS(COLUMS)) entregada[$];
+  //drv_chckr	#(.pckg_sz (pckg_sz ),.ROWS(ROWS),.COLUMS(COLUMS)) arr_ent [int];
   
   function new();
     this.recibida= {};
-    //this.entregada= {};
+    this.entregada= {};
   endfunction 
   
 // Tarea para el scoreboard del monitor
   
-  task run(); 
+  task run_rec(); 
     forever begin
       this.mntr_score_mbx.get(this.mntr_score_transaccion); // trae el la información del monitor al scoreboard
       this.recibida.push_back(this.mntr_score_transaccion); // Agarra esa información  y la almacena en la cola
       
       arr_rec [this.mntr_score_transaccion.dato]=this.mntr_score_transaccion;
       
-      $display(" Se recibió el dato %b  con id %g con un tiempo de envío de 	[%g] y con modo[%g]",this.mntr_score_transaccion.dato,this.mntr_score_transaccion.id,this.mntr_score_transaccion.tiempo, this.mntr_score_transaccion.modo);
+      $display(" Se recibió del monitor el dato %b  con id %g con un tiempo de envío de 	[%g] y con modo[%g]",this.mntr_score_transaccion.dato,this.mntr_score_transaccion.id,this.mntr_score_transaccion.tiempo, this.mntr_score_transaccion.modo);
     end 
   endtask
   
@@ -51,14 +51,14 @@ class scoreboard #( parameter ROWS = 4, parameter COLUMS = 4, parameter pckg_sz 
   
   // Tarea para el scoreboard del agente 
 
-/*task run(); 
+task run_ent(); 
     forever begin
-      this.drv_chckr_mbx.get(this.drv_chckr_transaccion); // trae el la información del minitor al scoreboard
-      this.entregada.push_back(this.drv_chckr_transaccion); // Agarra esa información  y la almacena en la cola
+      this.drv_chckr_mbx.get(this.drv_chckr_transaccion_); // trae el la información del driver al scoreboard
+      this.entregada.push_back(this.drv_chckr_transaccion_); // Agarra esa información  y la almacena en la cola
       
-      $display(" Se recibió el dato %b  con id %b con un tiempo de envío de [%g]",this.drv_chckr_transaccion.dato,this.drv_chckr_transaccion.id, this.drv_chckr_transaccion.tiempo);
+      $display(" Se recibió del driver el dato %b  con id %b con un tiempo de envío de [%g]",this.drv_chckr_transaccion_.dato,this.drv_chckr_transaccion_.id, this.drv_chckr_transaccion_.tiempo, this.drv_chckr_transaccion_.modo);
     end 
   endtask
   
-  */
+  
 endclass
