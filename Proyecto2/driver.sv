8444b6f41fa99304ecb99634b8e6d4386f424697
reimorales1 <100234823+reimorales1@users.noreply.github.com>
2023-10-29 18:50:05 -0600

    driver.sv casi final

--- Proyecto2/driver.sv
+++ Proyecto2/driver.sv
@@ -1,17 +1,18 @@
 `include "interface.sv"
 `include "clases.sv"
-`include "fifo_dr.sv"
+`include "fifo_in.sv"
 
-// Driver: fuerza senales al DUT
-
-class Driver #(parameter drvrs = 16, parameter rows = 4, parameter columns = 4, parameter pckg_sz = 40, parameter f_depth = 4);
-  int driver_num;
+class Driver #(parameter drvrs = 16, parameter ROWS = 4, parameter COLUMS = 4, parameter pckg_sz = 40, parameter fifo_depth = 4);
+  logic [3:0] num_driver;
+  int hold;
   
-  fifo_dr #(.rows(rows), .columns(columns), .pckg_sz(pckg_sz), .f_depth(f_depth)) fifo_dr_d;
-  agent_driver_mb #(.rows(rows), .columns(columns), .pckg_sz(pckg_sz)) agent_driver_mb_d;
-  trans_agent_driver #(.rows(rows), .columns(columns), .pckg_sz(pckg_sz)) trans_agent_driver_d;
+  fifo_dr #(.ROWS(ROWS), .COLUMS(COLUMS), .pckg_sz(pckg_sz), .fifo_depth(fifo_depth)) fifo_in_d;
+  agent_driver_mbx #(.ROWS(ROWS), .COLUMS(COLUMS), .pckg_sz(pckg_sz)) agent_driver_mbx_d;
+  agent_driver #(.ROWS(ROWS), .COLUMS(COLUMS), .pckg_sz(pckg_sz)) agent_driver_d;
   
-  /////////Mailbox////////////
+  
+  
+   /////////Mailbox////////////
   
   drv_chckr_mbx drv_chckr_mbx;
   
@@ -21,43 +22,93 @@ class Driver #(parameter drvrs = 16, parameter rows = 4, parameter columns = 4, 
   
   drv_chckr transaccion;
   
-  function new(int dn);// Constructor de la clase Driver
-    this.driver_num = dn;
-    this.fifo_dr_d = new(driver_num);
-    this.agent_driver_mb_d = new();
-    this.trans_agent_driver_d = new();
-    $display ("Driver [%g] inicializado en %g ", driver_num, $time);
+  
+  
+ 
+  function new(int ID);
+    this.num_driver = ID; 
+    this.fifo_in_d = new(num_driver);
+    this.agent_driver_mbx_d = new();
+    this.agent_driver_d = new();
+    $display ("Driver [%g] ha inicializado en %g", num_driver, $time);
   endfunction
+  
+  
+          // Declaración de una función que toma dos entradas y devuelve una salida
+  function logic [3:0] Destino(input logic [3:0] fila, input logic [3:0] columna);
+    logic [3:0] destino;
+      // Realiza alguna operación basada en las entradas
+           case({fila,columna})
+              8'b0000_0001: destino = 4'b0000;  // 0
+              8'b0000_0010: destino = 4'b0001;  // 1
+              8'b0000_0011: destino = 4'b0010;  // 2
+              8'b0000_0100: destino = 4'b0011;  // 3
+              8'b0001_0000: destino = 4'b0100;  // 4
+              8'b0010_0000: destino = 4'b0101;  // 5
+              8'b0011_0000: destino = 4'b0110;  // 6
+              8'b0100_0000: destino = 4'b0111;  // 7
+              8'b0101_0001: destino = 4'b1000;  // 8
+              8'b0101_0010: destino = 4'b1001;  // 9
+              8'b0101_0011: destino = 4'b1010;  // 10
+              8'b0101_0100: destino = 4'b1011;  // 11
+              8'b0001_0101: destino = 4'b1100;  // 12
+              8'b0010_0101: destino = 4'b1101;  // 13
+              8'b0011_0101: destino = 4'b1110;  // 14
+              8'b0100_0101: destino = 4'b1111;  // 15       
+            default: destino = 4'b1111; // Cuando selector2:selector1 no coincide con ninguno de los casos            
+          endcase
+      return destino;
+    endfunction
   
   virtual task run();
     fork
-      fifo_dr_d.interfaz(); // Iniciar la interfaz de la FIFO
+      fifo_in_d.interfaz();
     join_none
     
     forever begin
-      this.agent_driver_mb_d.get(trans_agent_driver_d);                                         
-      $display("Driver [%g] recibida transaccion en %g ",this.driver_num, $time );
-      $display("Driver [%g] envia dato %b a fila [%g] y columna [%g]",this.driver_num,this.trans_agent_driver_d.dato,this.trans_agent_driver_d.c_fila,this.trans_agent_driver_d.c_columna);
+      this.agent_driver_mbx_d.get(agent_driver_d);                                          
+      $display("Driver [%g] ha recibido una transaccion en %g",this.num_driver, $time);
+      $display("Driver [%g] ha enviado el dato %b",this.num_driver,this.agent_driver_d.dato);
+      $display("Destino con external ID [%d] [%d] = [%g] ",this.agent_driver_d.fila_,this.agent_driver_d.columna_,this.Destino(this.agent_driver_d.fila_,this.agent_driver_d.columna_));
+      $display("Tiempo de retardo = [%g] ",this.agent_driver_d.retardo);
+      $display("Modo = [%d] \n ",this.agent_driver_d.mode);
       
+    //////////////////////////////////////////////////////////////  
       
               this.transaccion=new();
-              this.transaccion.dato= this.trans_agent_driver_d.dato;
-              this.transaccion.c_fila= this.trans_agent_driver_d.c_fila;
-              this.transaccion.c_columna =this.trans_agent_driver_d.c_columna;
+              this.transaccion.num_driver= this.num_driver;
+              this.transaccion=new();
+              this.transaccion.num_driver= this.num_driver;
+              this.transaccion.dato= this.agent_driver_d.dato;
+              this.transaccion.c_fila= this.agent_driver_d.fila_;
+              this.transaccion.c_columna =this.agent_driver_d.columna_;
+      this.transaccion.Destino =this.Destino(this.agent_driver_d.fila_,this.agent_driver_d.columna_);
               //this.transaccion.tiempo= $time;
-              this.transaccion.modo= this.trans_agent_driver_d.modo;
-      
-      
-      
-      //this.drv_chckr_mbx.put({this.trans_agent_driver_d.Next_jump,this.trans_agent_driver_d.c_fila,this.trans_agent_driver_d.c_columna,this.trans_agent_driver_d.modo,this.trans_agent_driver_d.dato});
+              this.transaccion.modo= this.agent_driver_d.mode;
+     
       
               drv_chckr_mbx.put(transaccion);
+    ///////////////////////////////////////////////////////////////////////
       
-      while(this.fifo_dr_d.FIFO_IN.size >= f_depth) #5;
-      this.fifo_dr_d.Fin_push({this.trans_agent_driver_d.Next_jump,this.trans_agent_driver_d.c_fila,this.trans_agent_driver_d.c_columna,this.trans_agent_driver_d.modo,this.trans_agent_driver_d.dato});
+      
+      hold=0;
+      while(hold< agent_driver_d.retardo)begin
+        @(posedge fifo_in_d.interf.clk);
+        hold=hold+1;
+      end
+      
+      while(this.fifo_in_d.fifo_emul.size >= fifo_depth) #5;
+     
+      
+ this.fifo_in_d.Fin_push({this.agent_driver_d.Next_jump,this.agent_driver_d.fila_,this.agent_driver_d.columna_,this.agent_driver_d.mode,this.num_driver,Destino(this.agent_driver_d.fila_,this.agent_driver_d.columna_),this.agent_driver_d.dato});   
+      
+      
+      
+      
       
       
     end
   endtask
 endclass
+
 
